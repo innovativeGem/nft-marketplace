1c9ba06e6908c0034b1391cb72ea857ebe77659e
innovativeGem <innovativeGem@gmail.com>
2024-04-09 15:39:00 +1000

    [0002]-BaseLayout and components

--- components/layout/BaseLayout.tsx
+++ components/layout/BaseLayout.tsx
@@ -1,14 +1,18 @@
 import { FunctionComponent } from 'react';
+import Navbar from '../navbar';
 
 type Props = { children: any };
 
 const BaseLayout: FunctionComponent<Props> = ({ children }) => {
   return (
-    <div className='py-16 bg-gray-50 overflow-hidden min-h-screen'>
-      <div className='max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8'>
-        {children}
+    <>
+      <Navbar />
+      <div className='py-16 bg-gray-50 overflow-hidden min-h-screen'>
+        <div className='max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8'>
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
